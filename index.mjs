f8af663984990ce6615106c5f0ff2a834bdebded
johackim <contact@johackim.com>
2023-12-26 18:42:25 +0100

    feat: update

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
-const currentDate = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000));
+const currentDate = new Date(new Date().getTime() + (new Date().getTimezoneOffset() * 60000) + (3600000 * 1));
 
 console.log({ currentDate });
